847b6cf13859187e552759242059f5d10d3283ff
Enjamul Islam <mdenjamulislam527@gmail.com>
2023-12-13 21:26:35 +0600

    Added Hourly forecast chart

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { LineChart, ResponsiveContainer, XAxis } from "recharts";
+import { Legend, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
 
 const Forecast = ({ apiKey, baseUrl, currentCountry }) => {
     const [forecastData, setForecastData] = useState([null]);
@@ -8,7 +8,7 @@ const Forecast = ({ apiKey, baseUrl, currentCountry }) => {
     const apiEndpoint = baseUrl + endPoint;
 
     useEffect(() => {
-        const fetchWeatherForecast  = async () => {
+        const fetchWeatherForecast = async () => {
             try {
                 const response = await axios.get(`${apiEndpoint}?key=${apiKey}&q=${currentCountry}&aqi=no`);
                 setForecastData(response.data);
@@ -17,9 +17,7 @@ const Forecast = ({ apiKey, baseUrl, currentCountry }) => {
             }
         };
         fetchWeatherForecast();
-    },[ apiKey, apiEndpoint, currentCountry ]);
-
-    console.log(forecastData);
+    }, [apiKey, apiEndpoint, currentCountry]);
 
     let { forecast } = forecastData;
     let forecastday;
@@ -28,13 +26,37 @@ const Forecast = ({ apiKey, baseUrl, currentCountry }) => {
         ({ forecast } = forecastData);
         ({ forecastday } = forecast);
     }
-    
-    console.log(typeof(forecastday));
+    console.log(forecastday);
+
+    let chartData = [];
+    if (forecastday && Array.isArray(forecastday)) {
+        forecastday.map(datas => (
+            chartData = datas.hour.map(data => {
+                let hour = data.time.split(' ')[1].split(':')[0];
+                return {
+                    time: hour,
+                    temp_c: data.temp_c,
+                    temp_f: data.temp_f,
+                };
+            })
+        ));
+    }
+    // parseInt(phone.slug.split('-')[1])
+
+    console.log(chartData);
+
+
     return (
         <div>
-            <h2 className="text-xl md:text-2xl font-bold pb-3 text-white">5-day weather forecast</h2>
+            <h2 className="text-xl md:text-2xl font-bold pb-3 text-white">24 Hours weather forecast</h2>
             <div className="p-4 md:p-6 border border-gray-400 rounded-xl bg-glass">
-                
+                <LineChart width={600} height={250} data={chartData}>
+                    <XAxis dataKey="time"></XAxis>
+                    <YAxis />
+                    <Tooltip />
+                    <Line type="monotone" dataKey="temp_c" stroke="#fff"></Line>
+                    <Line type="monotone" dataKey="temp_f" stroke="#43Dc"></Line>
+                </LineChart>
             </div>
         </div>
     );
